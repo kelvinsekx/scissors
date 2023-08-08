ea95d58ded0fb53da38fe818fc940b6295d7c944
Blessing Adediwura <blessingadediwura97@gmail.com>
2023-08-08 07:38:34 +0100

    Update env.mjs

--- src/env.mjs
+++ src/env.mjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
@@ -13,19 +14,18 @@ export const env = createEnv({
       process.env.NODE_ENV === "production"
         ? z.string().min(1)
         : z.string().min(1).optional(),
-    NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string().min(1) : z.string().url()
-    ),
-      // @ts-ignore
-      // @ts-ignore
-      // @ts-ignore
-    .optional(),
-    NEXT_PUBLIC_SUPABASE_URL: z.string().min(1),
-    SUPABASE_SERVICE_ROLE_KEY: z.string().min(1),
+    NEXTAUTH_URL: z
+      .preprocess(
+        // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+        // Since NextAuth.js automatically uses the VERCEL_URL if present.
+        (str) => process.env.VERCEL_URL ?? str,
+        // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+        process.env.VERCEL ? z.string().min(1) : z.string().url()
+      )
+      .optional(),
+    NEXT_PUBLIC_SUPABASE_URL: "https://fxyweoeybgkegjkptjfk.supabase.co",
+    SUPABASE_SERVICE_ROLE_KEY:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4eXdlb2V5YmdrZWdqa3B0amZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgwMzMxMTksImV4cCI6MjAwMzYwOTExOX0.nfCPGmmi9WM0q7TV8Yl80RfFP0ubqif5lTzD-XtlaHQ",
   },
 
   /**
@@ -41,7 +41,7 @@ export const env = createEnv({
    * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
    * middlewares) or client-side so we need to destruct manually.
    */
-  // @ts-ignore
+
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
