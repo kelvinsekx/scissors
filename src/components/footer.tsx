0f2233b2d11bf5fd39857a44311b3c92cbc2194c
Blessing Adediwura <blessingadediwura97@gmail.com>
2023-08-09 16:20:11 +0100

    Eureka!!
    Landing page doneeeee

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,180 +3,201 @@ import React from "react";
 export function Footer() {
   return (
     <div className="relative">
-       <img className="w-[24px] h-[24px] absoute top-0 right-0 "  src="/Vector (1).svg" alt="svg" /> 
-        <img className="w-[24px] h-[24px] absolute bottom-0 left-0"  src="/Vector (2).svg" alt="svg" />
-        <div className="relative h-[561px] w-[1440px] mx-auto bg-white overflow-hidden">
-          <div className="absolute pb-[82px] left-[426px] top-[95px] inline-flex items-start justify-start gap-6">
-            <div className="inline-flex flex-col items-start justify-start gap-14">
-              <div className="flex flex-col items-start justify-start gap-2 px-6">
-                <div className="text-[16px] font-[700] leading-normal text-[#071827]">
-                  Why Scissor ?
-                  <div>
-
-                  </div>
-                </div>
-                <div className="inline-flex items-start justify-start">
-                  <div className="text-[16px] font-[500] leading-normal text-[#071827]">
-                    Scissor 101
-                    <br />
-                    Integrations & API
-                    <br />
-                    Pricing
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col items-start justify-start gap-2 px-6">
-                <div className="text-[16px]  text-[#071827] font-[700] leading-normal">
-                  Resources
-                </div>
-                <div className="inline-flex items-start justify-start">
-                  <div className="text-[16px] font-[500] leading-normal text-[#071827]">
-                    Blog
-                    <br />
-                    Resource Library
-                    <br />
-                    Developers
-                    <br />
-                    App Connectors
-                    <br />
-                    Support
-                    <br />
-                    Trust Center
-                    <br />
-                    Browser Extension
-                    <br />
-                    Mobile App
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="inline-flex flex-col items-start justify-start gap-[31px]">
-              <div className="flex flex-col items-start justify-start gap-2 px-6">
-                <div className="text-[16px] font-[700] leading-normal  text-[#071827]">
-                  Solutions
-                </div>
-                <div className="inline-flex items-start justify-start">
-                  <div className="text-[16px] font-[500] leading-normal text-[#071827]">
-                    Social Media
-                    <br />
-                    Digital Marketing
-                    <br />
-                    Customer Service
-                    <br />
-                    For Developers
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col items-start justify-start gap-2 px-6">
-                <div className="text-[16px] font-[700] leading-normal  text-[#071827]">
-                  Features
-                </div>
-                <div className="inline-flex items-center justify-start">
-                  <div className="text-[16px] font-[500] leading-normal text-[#071827]">
-                    Branded Links
-                    <br />
-                    Mobile Links
-                    <br />
-                    Campaign
-                    <br />
-                    Management &<br />
-                    Analytics
-                    <br />
-                    QR Code generation
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="inline-flex flex-col items-start justify-start gap-14">
-              <div className="flex flex-col items-start justify-start gap-2 px-6">
-                <div className="text-[16px] font-[700] leading-normal  text-[#071827]">
-                  Products
-                </div>
-                <div className="inline-flex items-start justify-start">
-                  <div className="text-[16px] font-[500] leading-normal text-[#071827]">
-                    Link Management
-                    <br />
-                    QR Codes
-                    <br />
-                    Link-in-bio
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col items-start justify-start gap-2 px-6">
-                <div className="text-[16px] font-[700] leading-normal  text-[#071827]">
-                  Legal
-                </div>
-                <div className="inline-flex items-start justify-start">
-                  <div className="text-[16px] font-[500] leading-normal text-[#071827]">
-                    Privacy Policy
-                    <br />
-                    Cookie Policy
-                    <br />
-                    Terms of Service
-                    <br />
-                    Acceptable Use Policy
-                    <br />
-                    Code of Conduct
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="inline-flex flex-col items-start justify-start gap-2 px-6">
-              <div className="h-5 w-[94px] text-[16px] font-[700] leading-normal  text-[#071827]">
-                Company
+      <img
+        className="absoute right-0 top-0 h-[24px] w-[24px] "
+        src="/Vector (1).svg"
+        alt="svg"
+      />
+      <img
+        className="absolute bottom-0 left-0 h-[24px] w-[24px]"
+        src="/Vector (2).svg"
+        alt="svg"
+      />
+      <div className="relative mx-auto h-[561px] w-[1440px] overflow-hidden bg-white">
+        <div className="absolute left-[326px] top-[95px] inline-flex items-start justify-start gap-6 pb-[82px]">
+          <div className="inline-flex flex-col items-start justify-start gap-14">
+            <div className="flex flex-col items-start justify-start gap-2 px-6">
+              <div className="text-[16px] font-[700] leading-normal text-[#071827]">
+                Why Scissor ?<div></div>
               </div>
               <div className="inline-flex items-start justify-start">
                 <div className="text-[16px] font-[500] leading-normal text-[#071827]">
-                  About Scissor
+                  Scissor 101
                   <br />
-                  Careers
+                  Integrations & API
                   <br />
-                  Partners
+                  Pricing
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col items-start justify-start gap-2 px-6">
+              <div className="text-[16px]  font-[700] leading-normal text-[#071827]">
+                Resources
+              </div>
+              <div className="inline-flex items-start justify-start">
+                <div className="text-[16px] font-[500] leading-normal text-[#071827]">
+                  Blog
                   <br />
-                  Press
+                  Resource Library
                   <br />
-                  Contact
+                  Developers
                   <br />
-                  Reviews
+                  App Connectors
+                  <br />
+                  Support
+                  <br />
+                  Trust Center
+                  <br />
+                  Browser Extension
+                  <br />
+                  Mobile App
                 </div>
               </div>
             </div>
           </div>
-          <div className="absolute left-[203px] top-[95px] inline-flex flex-col items-start justify-start gap-8">
-            <div className="flex items-center justify-center gap-2 p-2">
-              <img className="w-[24px] h-[24px] "  src="/vector.svg" alt="svg" /> 
-              <img className="w-[24px] h-[24px] "  src="/vector 2.svg" alt="svg" /> 
-              <div className=" w-[99px] text-[24px] font-[700] leading-loose text-[#141414]">
-                SCISSOR
+          <div className="inline-flex flex-col items-start justify-start gap-[31px]">
+            <div className="flex flex-col items-start justify-start gap-2 px-6">
+              <div className="text-[16px] font-[700] leading-normal  text-[#071827]">
+                Solutions
+              </div>
+              <div className="inline-flex items-start justify-start">
+                <div className="text-[16px] font-[500] leading-normal text-[#071827]">
+                  Social Media
+                  <br />
+                  Digital Marketing
+                  <br />
+                  Customer Service
+                  <br />
+                  For Developers
+                </div>
               </div>
             </div>
-            <div className="flex gap-[24px]">
-              <img className="w-[24px] h-[24px] "  src="/i.fi-social-twitter.svg" alt="svg" /> 
-              <img className="w-[24px] h-[24px] "  src="/svg.feather.svg" alt="svg" /> 
-              <img className="w-[24px] h-[24px] "  src="/i.fi-social-linkedin.svg" alt="svg" /> 
-              <img className="w-[24px] h-[24px] "  src="/i.fi-social-facebook.svg" alt="svg" /> 
-            </div>
-            <div className="inline-flex items-start justify-start gap-6 px-2">
-              <div className="relative h-6 w-6" />
-              <div className="relative h-6 w-6" />
-              <div className="relative h-6 w-6" />
-              <div className="relative h-6 w-6" />
+            <div className="flex flex-col items-start justify-start gap-2 px-6">
+              <div className="text-[16px] font-[700] leading-normal  text-[#071827]">
+                Features
+              </div>
+              <div className="inline-flex items-center justify-start">
+                <div className="text-[16px] font-[500] leading-normal text-[#071827]">
+                  Branded Links
+                  <br />
+                  Mobile Links
+                  <br />
+                  Campaign
+                  <br />
+                  Management &<br />
+                  Analytics
+                  <br />
+                  QR Code generation
+                </div>
+              </div>
             </div>
           </div>
-          <div className="absolute left-[897px] top-[309.27px] h-[596.14px] w-[804.53px] origin-top-left rotate-[-7.59deg] opacity-70"></div>
-          <div className="absolute left-[507.50px] top-[229.15px] h-[596.14px] w-[804.53px] origin-top-left rotate-[110.54deg] opacity-80"></div>
-          <div className="absolute left-[1017px] top-[502px] inline-flex items-center justify-center gap-2 px-3">
-            <div className="text-[14px] font-[500] leading-tight text-[#071827]">
-              Term of Service |
+          <div className="inline-flex flex-col items-start justify-start gap-14">
+            <div className="flex flex-col items-start justify-start gap-2 px-6">
+              <div className="text-[16px] font-[700] leading-normal  text-[#071827]">
+                Products
+              </div>
+              <div className="inline-flex items-start justify-start">
+                <div className="text-[16px] font-[500] leading-normal text-[#071827]">
+                  Link Management
+                  <br />
+                  QR Codes
+                  <br />
+                  Link-in-bio
+                </div>
+              </div>
             </div>
-            <div className="text-[14px] font-[500] leading-tight text-[#071827]">
-              Security |
+            <div className="flex flex-col items-start justify-start gap-2 px-6">
+              <div className="text-[16px] font-[700] leading-normal  text-[#071827]">
+                Legal
+              </div>
+              <div className="inline-flex items-start justify-start">
+                <div className="text-[16px] font-[500] leading-normal text-[#071827]">
+                  Privacy Policy
+                  <br />
+                  Cookie Policy
+                  <br />
+                  Terms of Service
+                  <br />
+                  Acceptable Use Policy
+                  <br />
+                  Code of Conduct
+                </div>
+              </div>
             </div>
-            <div className="text-[14px] font-[500] leading-tight text-[#071827]">
-              ⓒ Scissor 2023
+          </div>
+          <div className="inline-flex flex-col items-start justify-start gap-2 px-6">
+            <div className="h-5 w-[94px] text-[16px] font-[700] leading-normal  text-[#071827]">
+              Company
+            </div>
+            <div className="inline-flex items-start justify-start">
+              <div className="text-[16px] font-[500] leading-normal text-[#071827]">
+                About Scissor
+                <br />
+                Careers
+                <br />
+                Partners
+                <br />
+                Press
+                <br />
+                Contact
+                <br />
+                Reviews
+              </div>
             </div>
           </div>
         </div>
+        <div className="absolute left-[93px] top-[95px] inline-flex flex-col items-start justify-start gap-8">
+          <div className="flex items-center justify-center gap-2 p-2">
+            <img className="h-[24px] w-[24px] " src="/vector.svg" alt="svg" />
+            <img className="h-[24px] w-[24px] " src="/vector 2.svg" alt="svg" />
+            <div className=" w-[99px] text-[24px] font-[700] leading-loose text-[#141414]">
+              SCISSOR
+            </div>
+          </div>
+          <div className="flex gap-[24px]">
+            <img
+              className="h-[24px] w-[24px] "
+              src="/i.fi-social-twitter.svg"
+              alt="svg"
+            />
+            <img
+              className="h-[24px] w-[24px] "
+              src="/svg.feather.svg"
+              alt="svg"
+            />
+            <img
+              className="h-[24px] w-[24px] "
+              src="/i.fi-social-linkedin.svg"
+              alt="svg"
+            />
+            <img
+              className="h-[24px] w-[24px] "
+              src="/i.fi-social-facebook.svg"
+              alt="svg"
+            />
+          </div>
+          <div className="inline-flex items-start justify-start gap-6 px-2">
+            <div className="relative h-6 w-6" />
+            <div className="relative h-6 w-6" />
+            <div className="relative h-6 w-6" />
+            <div className="relative h-6 w-6" />
+          </div>
+        </div>
+        <div className="absolute left-[897px] top-[309.27px] h-[596.14px] w-[804.53px] origin-top-left rotate-[-7.59deg] opacity-70"></div>
+        <div className="absolute left-[507.50px] top-[229.15px] h-[596.14px] w-[804.53px] origin-top-left rotate-[110.54deg] opacity-80"></div>
+        <div className="absolute left-[957px] top-[502px] inline-flex items-center justify-center gap-2 px-3">
+          <div className="text-[14px] font-[500] leading-tight text-[#071827]">
+            Term of Service |
+          </div>
+          <div className="text-[14px] font-[500] leading-tight text-[#071827]">
+            Security |
+          </div>
+          <div className="text-[14px] font-[500] leading-tight text-[#071827]">
+            ⓒ Scissor 2023
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
